bcfc58f011d61b6d1631c07bd1c1b9fc48663282
hamadbakeel <hamadbakeel@gmail.com>
2024-01-26 23:12:18 +0300

    new: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,37 @@ function App() {
           </div>
         </aside>
         <div className="page">
-          <header className='header'></header>
+          <header className='header'>
+            <ul className="links">
+              <li>
+                <a href="#">Overview</a>
+              </li>
+              <li>
+                <a href="#">Invest</a>
+              </li>
+              <li>
+                <a href="#" className='active'>Auto Invest</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+            </ul>
+
+            <div className="actions">
+              <button className="language">
+                En
+              </button>
+
+              <button className="notifications">
+                <img src="./bill.svg" alt="" />
+                <span className="count">2</span>
+              </button>
+
+              <button className='avatar'>
+                <img src="./Avatar.svg" alt="" />
+              </button>
+            </div>
+          </header>
           <div className="content"></div>
         </div>
       </div>
