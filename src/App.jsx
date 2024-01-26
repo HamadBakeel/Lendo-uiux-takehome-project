0d467aab753dc0eb89ae5475806dcecc2e95ee86
hamadbakeel <hamadbakeel@gmail.com>
2024-01-27 02:04:35 +0300

    new: 2 stats cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,129 @@
+import React, { Component } from 'react';
 import './Scss/App.scss'
+import ApexCharts from 'apexcharts'
+import Chart from 'react-apexcharts'
+import ReactApexChart from 'react-apexcharts';
+
+class LinesChart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          id: 'apexchart-example'
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+        },
+        yaxis: {
+          categories: ['0', '10k SAR', '20k SAR', '30k SAR'],
+          labels: {
+            formatter: function (value) {
+              return value + 'K SAR';
+            }
+          }
+        },
+        dataLabels: {
+          enabled: false // Disable data labels inside the columns
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 5, // Set border radius to make columns a bit rounded
+            horizontal: false,
+            stacked: true // Enable stacking to hide the second series column
+          }
+        },
+        chart: {
+          toolbar: {
+            show: false // Hide the download option
+          },
+          offsetY: 20,
+        }
+      },
+      series: [{
+        name: 'spent',
+        data: [4, 25, 18, 10, 26, 19, 0],
+      },
+      {
+        name: 'expected',
+        data: [null, null, null, null, null, null, 30], // Use null for the series you want to hide
+      },
+    ]
+    }
+  }
+
+  render() {
+    return (
+      <Chart options={this.state.options} series={this.state.series} type="bar" width={'100%'} height={194} />
+    )
+  }
+}
+
+class ApexChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [20, 30, 20, 30],
+      options: {
+        chart: {
+          type: 'donut',
+          offsetY: 20, // Adjust the value to set the desired top margin
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 144,
+            },
+            legend: {
+              position: 'bottom',
+            },
+          },
+        }],
+        dataLabels: {
+          enabled: false, // Enable data labels
+        },
+        plotOptions: {
+          pie: {
+            // offsetY: 20, // Adjust the value to set the desired top margin for the pie chart
+            offsetX: -60, // Adjust the value to set the desired left alignment for the pie chart
+          },
+        },
+        labels: ['A', 'B', 'C', 'D'], // Custom names for each partition
+        colors: ['rgba(255, 103, 105, 1)', 'rgba(75, 223, 255, 1)', 'rgba(255, 195, 89, 1)', 'rgba(45, 145, 254, 1)'], // Custom colors for each partition
+        legend: {
+          show: true,
+          position: 'right',
+          offsetX: 20,
+          formatter: function (seriesName, opts) {
+            const percent = opts.w.config.labels[opts.seriesIndex] + ' - ' + opts.w.config.series[1] + '%';
+            return '<div style="display: flex; justify-content: space-between; align-items: center;"><span style="margin-left: 5px;">' + percent + '</span></div>';
+          },
+        },
+      },
+    };
+    
+    
+    
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <div id="chart">
+          <ReactApexChart options={this.state.options} series={this.state.series} type="donut" height={150} />
+        </div>
+        <div id="html-dist"></div>
+      </div>
+    );
+  }
+}
+
+
 
 function App() {
 
@@ -139,6 +264,26 @@ function App() {
                       Change Settings
                     </button>
                   </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="stats">
+              <div className="card">
+                <h2 className="cardTitle">
+                Invested
+                <img src="./help.svg" alt="" />
+                </h2>
+                <div className="cardChart">
+                  <LinesChart/>
+                </div>
+              </div>
+              <div className="card">
+                <h2 className="cardTitle">
+                  Excepted Risk Score Distribution
+                </h2>
+                <div className="cardChart">
+                  <ApexChart/>
                 </div>
               </div>
             </div>
