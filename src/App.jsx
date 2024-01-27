7c9711714bcdbc796892d15b49513a730d8f68c4
hamadbakeel <hamadbakeel@gmail.com>
2024-01-27 03:18:36 +0300

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './Scss/App.scss'
-import ApexCharts from 'apexcharts'
 import Chart from 'react-apexcharts'
 import ReactApexChart from 'react-apexcharts';
 import Accordion from './Components/Accordion';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 class LinesChart extends Component {
   constructor(props) {
@@ -127,97 +128,21 @@ class ApexChart extends React.Component {
 
 
 function App() {
-
+  const [active, setActive] = useState(true)
   return (
     <>
       <div className="body">
-        <aside className='sidebar'>
-          <div className="logo">
-            <img src="./Logo.svg" alt="" />
-          </div>
-          <div className="content">
-            <div className="balanceBox">
-              <div className="numbers">
-                <img src="./balanceIcon.svg" alt="" />
-                <div className="text">
-                  <h2>
-                    <span>62,540 </span>
-                    <span>SAR</span>
-                  </h2>
-                  <div className='textIcon'>
-                    <p>Account Balance</p>
-                    <img className='chevronLeftIcon' src="./chevronLeftIcon.svg" alt="" />  
-                  </div>
-                </div>
-              </div>
-              <div className="buttons">
-                <button className="btn blue">
-                  Deposit
-                </button>
-                <button className="btn outline">
-                  Withdraw
-                </button>
-              </div>
-
-              <div className="warningText">
-                <img src="./warningIcon.svg" alt="Warning" />
-                <p>
-                  To withdraw more than <strong>10,000 SAR</strong> you need to <strong>verify</strong> your banking account.
-                </p>
-              </div>
-
-              <div className="saudiIllustration">
-                <img src="./SaudiManIllustration.svg" alt="" />
-                <h3>
-                  Why Should You Trust It?
-                </h3>
-                <p>
-                  Because of this and that. Anyway investing is very good
-                </p>
-              </div>
-            </div>
-          </div>
-        </aside>
+        <Sidebar/>
         <div className="page">
-          <header className='header'>
-            <ul className="links">
-              <li>
-                <a href="#">Overview</a>
-              </li>
-              <li>
-                <a href="#">Invest</a>
-              </li>
-              <li>
-                <a href="#" className='active'>Auto Invest</a>
-              </li>
-              <li>
-                <a href="#">Portfolio</a>
-              </li>
-            </ul>
-
-            <div className="actions">
-              <button className="language">
-                En
-              </button>
-
-              <button className="notifications">
-                <img src="./bill.svg" alt="" />
-                <span className="count">2</span>
-              </button>
-
-              <button className='avatar'>
-                <img src="./Avatar.svg" alt="" />
-              </button>
-            </div>
-          </header>
+          <Header/>
           <div className="content">
             <div className="autoInvestment">
               <h1>Auto Investment</h1>
 
               <div className="activate">
-                <span>Active</span>
+                <span>{active ? 'Active' : 'Not Active'}</span>
                 <label class="switch" for="checkbox">
-                  <input type="checkbox"  checked id="checkbox" />
+                  <input type="checkbox" onChange={()=>setActive(!active)} checked={active} id="checkbox" />
                   <div class="slider round"></div>
                 </label>
               </div>
@@ -241,7 +166,9 @@ function App() {
               <div className="cardContent">
                 <div className="top">
                   <div className="item">
-                    <h3 className='active'>Active</h3>
+                    <h3 className='active'>
+                      <span style={{color: !active ? 'red' : ''}}>{active ? 'Active' : 'Not Active'}</span>
+                    </h3>
                     <p>Status</p>
                   </div>
                   <div className="item">
